eb0a7ee27c3ec029b3276a8f482a1eab821c84af
Thufir <HAWAT.THUFIR@GMAIL.COM>
2012-04-25 16:03:18 -0700

    increment

--- src/net/bounceme/dur/nntp/Simple.java
+++ src/net/bounceme/dur/nntp/Simple.java
@@ -2,12 +2,10 @@ package net.bounceme.dur.nntp;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.awt.event.MouseEvent;
 import javax.swing.JFrame;
 import javax.swing.JSlider;
 import javax.swing.JTable;
 import javax.swing.JTextPane;
-import javax.swing.event.ChangeEvent;
 import javax.swing.table.DefaultTableModel;
 
 public class Simple {
@@ -23,25 +21,25 @@ public class Simple {
 
         table.addMouseListener(new java.awt.event.MouseAdapter() {
 
+            @Override
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                tableMouseClicked(evt);
-            }
-            private void tableMouseClicked(MouseEvent evt) {
-                throw new UnsupportedOperationException("Not yet implemented");
+                int row = table.rowAtPoint(evt.getPoint());
+                MessageBean mb = MessageUtils.getMessageBean(row);
+                text.setText(mb.getContent());
+                text.setContentType("text/html");
             }
         });
 
         slider.setMaximum(MessageUtils.getMax());
+        slider.setValue(MessageUtils.getMax());
         slider.addChangeListener(new javax.swing.event.ChangeListener() {
 
             public void stateChanged(javax.swing.event.ChangeEvent evt) {
-                jSlider1StateChanged(evt);
-            }
-
-            private void jSlider1StateChanged(ChangeEvent evt) {
                 int index = slider.getValue();
                 MessageUtils.setIndex(index);
                 dtm = MessageUtils.getDataTableModel();
+                table.setModel(dtm);
+                
                 f.revalidate();
                 f.repaint();
             }
