0bb58124ad33b7f8d7c2c0cd75bde04b46299bf3
Thufir <HAWAT.THUFIR@GMAIL.COM>
2012-04-25 18:10:45 -0700

    increment

--- src/net/bounceme/dur/nntp/Simple.java
+++ src/net/bounceme/dur/nntp/Simple.java
@@ -12,39 +12,23 @@ public class Simple {
 
     private static JFrame f = new JFrame();
     private static JSlider slider = new JSlider();
+    private static DefaultTableModel dtm = new DefaultTableModel();
 
     public static void main(String args[]) {
-        table.setModel(dtm);
 
-        table.addMouseListener(new java.awt.event.MouseAdapter() {
-
-            @Override
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                int row = table.rowAtPoint(evt.getPoint());
-                MessageBean mb = MessageUtils.getMessageBean(row);
-                text.setText(mb.getContent());
-                text.setContentType("text/html");
-            }
-        });
-
+        MessageUtils.init();
         slider.setMaximum(MessageUtils.getMax());
-        slider.setValue(MessageUtils.getMax());
+        //slider.setValue(MessageUtils.getMax());
         slider.addChangeListener(new javax.swing.event.ChangeListener() {
 
             public void stateChanged(javax.swing.event.ChangeEvent evt) {
                 int index = slider.getValue();
                 MessageUtils.setIndex(index);
-                dtm = MessageUtils.getDataTableModel();
-                table.setModel(dtm);
-                table.revalidate();
-                table.repaint();
+                //dtm = MessageUtils.getDataTableModel();
                 f.revalidate();
                 f.repaint();
             }
         });
-
-        f.add(table);
-        f.add(text);
         f.add(slider);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
