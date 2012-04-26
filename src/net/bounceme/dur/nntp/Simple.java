e1cb41e4ad82614b164aaeff728d2294fcfa15f8
Thufir <HAWAT.THUFIR@GMAIL.COM>
2012-04-26 00:46:30 -0700

    increment

--- src/net/bounceme/dur/nntp/Simple.java
+++ src/net/bounceme/dur/nntp/Simple.java
@@ -3,6 +3,7 @@ package net.bounceme.dur.nntp;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
+import java.util.logging.Logger;
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
@@ -10,48 +11,58 @@ import javax.swing.table.DefaultTableModel;
 
 public class Simple {
 
+    private static final Logger LOG = Logger.getLogger(Simple.class.getName());
     static JFrame frame = new JFrame();
     static JTextPane text = new JTextPane();
     static JSlider slider = new JSlider();
-    static MessagesController mc = new MessagesController();
-    static DefaultTableModel dtm = new DefaultTableModel();//mc.getDataTableModel();
+    static MessagesController messagesController = new MessagesController();
+    static DefaultTableModel defaultTableModel = new DefaultTableModel();
     static JTable table = new JTable();
 
     private static void createAndShowGUI() {
-        table.setModel(dtm);
+        table.setModel(defaultTableModel);
         table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
 
             public void valueChanged(ListSelectionEvent evt) {
-                int row = table.getSelectedRow();
-                MessageBean mb = mc.getMessageBean(row);
-                text.setText(mb.getContent());
-                text.setContentType("text/html");
-                text.revalidate();
-                text.repaint();
+                java.awt.EventQueue.invokeLater(new Runnable() {
+
+                    public void run() {
+                        int row = table.getSelectedRow();
+                        MessageBean messageBean = messagesController.getMessageBean(row);
+                        text.setText(messageBean.getContent());
+                        text.setContentType("text/html");
+                    }
+                });
             }
         });
 
-        slider.setMaximum(mc.getMax());
-        slider.setValue(mc.getMax());
+        slider.setMinimum(messagesController.getMax() - 50);
+        slider.setMaximum(messagesController.getMax());
+        slider.setValue(messagesController.getMax());
         slider.addChangeListener(new javax.swing.event.ChangeListener() {
 
             public void stateChanged(javax.swing.event.ChangeEvent evt) {
-                int index = slider.getValue();
-                mc.setIndex(index);
-                dtm = mc.getDataTableModel();
-                table.setModel(dtm);
+                java.awt.EventQueue.invokeLater(new Runnable() {
+
+                    public void run() {
+                        int index = slider.getValue();
+                        LOG.info("slider index " + index);
+                        messagesController.setIndex(index);
+                        defaultTableModel = messagesController.getDataTableModel();
+                        table.setModel(defaultTableModel);
+                    }
+                });
             }
         });
 
+        table.getSelectionModel().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         table.getSelectionModel().setSelectionInterval(1, 1);
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(3,1));
+        panel.setLayout(new GridLayout(3, 1));
         panel.add(table);
         panel.add(text);
         panel.add(slider);
         frame.add(panel);
-        table.revalidate();
-        table.repaint();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         frame.pack();
