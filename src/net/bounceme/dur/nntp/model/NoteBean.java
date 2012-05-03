ccca30736d07c98d269f129cf38252268670b837
Thufir <HAWAT.THUFIR@GMAIL.COM>
2012-05-03 05:11:51 -0700

    increment

--- src/net/bounceme/dur/nntp/model/NoteBean.java
+++ src/net/bounceme/dur/nntp/model/NoteBean.java
@@ -1,39 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package net.bounceme.dur.nntp.model;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.logging.Logger;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Temporal;
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
 
+/**
+ *
+ * @author thufir
+ */
 @Entity
+@Table(name = "NOTEBEAN", catalog = "nntp", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Notebean.findAll", query = "SELECT n FROM Notebean n"),
+    @NamedQuery(name = "Notebean.findById", query = "SELECT n FROM Notebean n WHERE n.id = :id"),
+    @NamedQuery(name = "Notebean.findByStamp", query = "SELECT n FROM Notebean n WHERE n.stamp = :stamp"),
+    @NamedQuery(name = "Notebean.findByNote", query = "SELECT n FROM Notebean n WHERE n.note = :note")})
 public class NoteBean implements Serializable {
-
     private static final long serialVersionUID = 1L;
-    private static final Logger LOG = Logger.getLogger(NoteBean.class.getName());
-    @Column
-    private String note;
-    @Temporal(javax.persistence.TemporalType.DATE)
-    private Date stamp = new Date();
     @Id
+    @Basic(optional = false)
+    @Column(name = "ID", nullable = false)
     private Long id;
+    @Column(name = "STAMP")
+    @Temporal(TemporalType.DATE)
+    private Date stamp;
+    @Column(name = "NOTE", length = 255)
+    private String note;
 
     public NoteBean() {
     }
 
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    @Override
-    public String toString() {
-        return getNote() + " dated:" + getStamp();
+    public NoteBean(Long id) {
+        this.id = id;
     }
 
     public Long getId() {
@@ -51,4 +55,38 @@ public class NoteBean implements Serializable {
     public void setStamp(Date stamp) {
         this.stamp = stamp;
     }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof NoteBean)) {
+            return false;
+        }
+        NoteBean other = (NoteBean) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "net.bounceme.dur.nntp.model.Notebean[ id=" + id + " ]";
+    }
+
 }
